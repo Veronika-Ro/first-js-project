ec8e57331d4b69b971db9aa540733cc6aa49a1ef
Veronika-Ro <79661110+Veronika-Ro@users.noreply.github.com>
2021-03-09 23:05:35 +0000

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+alert('Hello world');
+// Comments-Everything after the slashes is ignored
+
+/* This is a longer comment.
+It might explain, in detail, why we made a certain decision.
+Or, it could show examples about how to use the following code snippet. */
+
+let favoriteFood = 'Seafood Spaghetti';
+document.write (favoriteFood);

9dfc0e7663c901558d5c143168a4f9be7a2a4706
Veronika-Ro <79661110+Veronika-Ro@users.noreply.github.com>
2021-03-16 20:07:09 +0000

    Added for loop for pokemonlist

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 
 let pokemonList = [
-    { name: "Bulbasaur", height: 7, type: ['grass', 'poison'] },
-    { name: "Charmander", height: 6, type: ['fire'] },
-    { name: "Pikachu", height: 4, type: ['electric'] },
-  ];
+  { name: "Bulbasaur", height: 0.7, type: ['grass', 'poison'] },
+  { name: "Charmander", height: 0.6, type: ['fire'] },
+  { name: "Pikachu", height: 0.4, type: ['electric'] },
+];
+
+for (let i=0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 0.6){
+  document.write (pokemonList[i].name + " - " + pokemonList[i].type + " type, " + pokemonList[i].height + " meters tall. (Wow that's big!) ")
+} else {
+  document.write (pokemonList[i].name + " - " + pokemonList[i].type + " type, " + pokemonList[i].height + " meters tall. ")
+}
+}

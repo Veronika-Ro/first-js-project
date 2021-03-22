d66bae6c4d2a21ff3430294695ed5e8769b1b61e
Veronika-Ro <79661110+Veronika-Ro@users.noreply.github.com>
2021-03-22 20:52:54 +0000

    replaced for function with forEach in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
-
 let pokemonList = [
   { name: "Bulbasaur", height: 0.7, type: ['grass', 'poison'] },
   { name: "Charmander", height: 0.6, type: ['fire'] },
   { name: "Pikachu", height: 0.4, type: ['electric'] },
 ];
 
-for (let i=0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 0.6){
-  document.write (pokemonList[i].name + " - " + pokemonList[i].type + " type, " + pokemonList[i].height + " meters tall. (Wow that's big!) <br /> ")
+pokemonList.forEach(function(pokemon) {
+  if (pokemon.height > 0.6){
+  document.write (pokemon.name + " - " + pokemon.type + " type, " + pokemon.height + " meters tall. (Wow that's big!) <br /> ")
 } else {
-  document.write (pokemonList[i].name + " - " + pokemonList[i].type + " type, " + pokemonList[i].height + " meters tall. <br /> ")
+  document.write (pokemon.name + " - " + pokemon.type + " type, " + pokemon.height + " meters tall. <br /> ")
 }
-}
+});

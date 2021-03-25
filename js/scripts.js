4d57ec6c671a7a615953a221a5db3877892060ca
Veronika-Ro <79661110+Veronika-Ro@users.noreply.github.com>
2021-03-25 21:25:59 +0000

    DOM manipulation, added buttons and event listener

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,50 @@ let pokemonRepository = (function () {
   { name: "Bulbasaur", height: 0.7, type: ['grass', 'poison'] },
   { name: "Charmander", height: 0.6, type: ['fire'] },
   { name: "Pikachu", height: 0.4, type: ['electric'] },
-];
+  ];
+
+  function addListItem(pokemon){
+    let orderedList = document.querySelector ('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemonButton');
+     //append the butten and the list to their parents 
+    listItem.appendChild(button);
+    orderedList.appendChild(listItem);
+    button.addEventListener('click', function (event) {
+    showDetails (pokemon);
+    });
+  }
+
+   //execute the details of clicked pokemon on console 
+  function showDetails(pokemon){
+    console.log(pokemon);
+  }
 
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
 
+  //getAll execute the pokemonlist
   function getAll() {
     return pokemonList;
   }
 
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
-
 pokemonRepository.getAll().forEach(function(pokemon) {
-  if (pokemon.height > 0.6){
+pokemonRepository.addListItem (pokemon);
+});
+
+ /* Previous function- if (pokemon.height > 0.6){
   document.write (pokemon.name + " - " + pokemon.type + " type, " + pokemon.height + " meters tall. (Wow that's big!) <br /> ")
 } else {
   document.write (pokemon.name + " - " + pokemon.type + " type, " + pokemon.height + " meters tall. <br /> ")
 }
-});
+}); */
